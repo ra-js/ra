bd03eb04ec78344c1ad49bc018f0fd1bf4cc91ca
Pokhodyun Alexander <karbunkul@yourtask.ru>
2017-05-30 23:36:41 +0500

    noinspect

--- src/lib/ra-core.js
+++ src/lib/ra-core.js
@@ -2,15 +2,13 @@
  * Created by karbunkul on 22.05.17.
  */
 
-const result = require('./ra-result');
-
 class Ra {
-
   constructor() {
     this.definitions = [];
     this.types = [];
   }
 
+  // noinspection JSMethodCanBeStatic
   call(req, res) {
     res.send('Hello world! ' + req.method);
   }
